5e7bcb64f31b0d149b4f730c964ec5ed39f1a169
username322 <82874463+SwiterKLy@users.noreply.github.com>
2022-05-22 21:53:50 +0300

    Create Lab2.java

---
+++ Lab2.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+public class Main {
+    static void Func1() {
+        int x = 0;
+        int y = 0;
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Введіть x");
+                x = scanner.nextInt();
+                System.out.println("Введіть y");
+                y = scanner.nextInt();
+                if (x - y == 0) {
+                    throw new Exception("Ділення на нуль неможливе,введіть данні заново");
+                }
+                if (x - y < 0) {
+                    throw new Exception("Корінь з від'ємного числа не вичисляється");
+                }
+                System.out.println((Math.pow(x, 2) + Math.pow(y, 2)) / Math.sqrt(x - y));
+                return;
+            } catch (Exception ex) {
+                System.out.println(ex.toString());
+                continue;
+            }
+        }
+    }
+
+    static double Func2() {
+        int sum = 0;
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        Math.pow(sum, 2);
+        return sum;
+    }
+
+    static void Func3() {
+        int x = 0;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введіть x");
+            x = scanner.nextInt();
+            double sum = 1;
+            for (double i = 1; i <= 5; i++) {
+                for (double j = 1; j <= i; j++) {
+                    sum = sum * (i + Math.pow(x, j));
+                }
+            }
+            System.out.print("Результат функції:");
+            System.out.printf("%.2f",sum);
+            System.out.println();
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+            Func3();
+        }
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                int input = 0;
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Введіть номер завдання 1-3");
+                input = scanner.nextInt();
+                if (input >= 1 || input >= 3) {
+                    switch (input) {
+                        case (1): {
+                            System.out.println("1 завдання:");
+                            Func1();
+                            break;
+                        }
+                        case (2): {
+                            System.out.println("2 завдання:");
+                            System.out.println(Func2());
+                            break;
+                        }
+                        case (3): {
+                            System.out.println("3 завдання:");
+                            Func3();
+                            break;
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                System.out.println("Неправильне значення");
+                continue;
+            }
+
+        }
+    }
+}
